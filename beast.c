50c17975971745b4e72ef771311b7faa57904257
Peter Rust <peter@cornerstonenw.com>
2018-02-19 23:39:03 -0800

    Added error handling & randomly-generated blocks

--- beast.c
+++ beast.c
@@ -1,65 +1,119 @@
 #include "include/SDL.h"
 #include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
 
-int main(int num_args, char** args) {
-  SDL_Init(SDL_INIT_VIDEO);
+// forward-declaring functions
+int error(char* activity);
+
+int block_w = 15;
+int block_h = 20;
+float block_density = 0.4;
+
+
+int main(int num_args, char* args[]) {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    return error("initializing SDL");
+
+  SDL_DisplayMode dm;
+  if (SDL_GetDesktopDisplayMode(0, &dm) < 0)
+    return error("getting display mode");
+
+  srand(time(NULL));
+
+  // use density to determine # of blocks & allocate memory for them
+  // in one shot on the stack
+  // this is more efficient than walking through each x/y block position
+  // & deciding randomly whether to have a block there
+  int num_blocks_w = dm.w / block_w;
+  int num_blocks_h = dm.h / block_h;
+  int num_possible_blocks = num_blocks_w * num_blocks_h;
+  int num_blocks = num_possible_blocks * block_density;
+
+  SDL_Rect blocks[num_blocks];
+
+  for (int i = 0; i < num_blocks; i++) {
+    blocks[i].x = (rand() % num_blocks_w) * block_w;
+    blocks[i].y = (rand() % num_blocks_h) * block_h;
+    blocks[i].w = block_w;
+    blocks[i].h = block_h;
+  }
+
   SDL_Window *window;
-  window = SDL_CreateWindow("Beast", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE);
+  window = SDL_CreateWindow("Beast", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, dm.w, dm.h, SDL_WINDOW_RESIZABLE);
+  if (!window)
+    return error("creating window");
+  if (SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN) < 0)
+    return error("setting fullscreen");
 
-  SDL_Renderer* renderer = NULL;
-  renderer =  SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  if (!renderer)
+    return error("creating renderer");
 
-  SDL_Rect r;
-  r.x = 50;
-  r.y = 50;
-  r.w = 15;
-  r.h = 20;
+  SDL_Rect player;
+  player.x = 0;
+  player.y = 0;
+  player.w = block_w;
+  player.h = block_h;
 
-  SDL_Event evt;
   bool is_gameover = false;
 
   while (!is_gameover) {
-    if (SDL_PollEvent(&evt)) {
+    SDL_Event evt;
+    while (SDL_PollEvent(&evt)) {
       switch(evt.type) {
-      case SDL_QUIT:
-        is_gameover = true;
-        break;
-      case SDL_KEYDOWN:
-        switch (evt.key.keysym.sym) {
-          case SDLK_ESCAPE:
-            is_gameover = true;
-            break;
-          case SDLK_LEFT:
-            r.x -= 15;
-            break;
-          case SDLK_RIGHT:
-            r.x += 15;
-            break;
-          case SDLK_UP:
-            r.y -= 20;
-            break;
-          case SDLK_DOWN:
-            r.y += 20;
-            break;
+        case SDL_QUIT:
+          is_gameover = true;
+          break;
+        case SDL_KEYDOWN:
+          switch (evt.key.keysym.sym) {
+            case SDLK_ESCAPE:
+              is_gameover = true;
+              break;
+            case SDLK_LEFT:
+              player.x -= player.w;
+              break;
+            case SDLK_RIGHT:
+              player.x += player.w;
+              break;
+            case SDLK_UP:
+              player.y -= player.h;
+              break;
+            case SDLK_DOWN:
+              player.y += player.h;
+              break;
         }
         break;
       }
     }
 
     // set BG color
-    SDL_SetRenderDrawColor(renderer, 150, 150, 150, 255);
-    SDL_RenderClear(renderer);
+    if (SDL_SetRenderDrawColor(renderer, 150, 150, 150, 255) < 0)
+      return error("setting bg color");
+    if (SDL_RenderClear(renderer) < 0)
+      return error("clearing renderer");
 
     // set block color
-    SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
-    SDL_RenderFillRect(renderer, &r);
+    if (SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255) < 0)
+      return error("setting block color");
+    if (SDL_RenderFillRect(renderer, &player) < 0)
+      return error("filling rect");
+
+    for (int i = 0; i < num_blocks; i++)
+      if (SDL_RenderFillRect(renderer, &blocks[i]) < 0)
+        return error("drawing block");
     
     SDL_RenderPresent(renderer);
     SDL_Delay(10);
-//    SDL_UpdateRect(screen, 0, 0, 0, 0);
   }
 
   SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
+}
+
+int error(char* activity) {
+  printf("%s failed: %s\n", activity, SDL_GetError());
+  SDL_Quit();
+  return -1;
 }
